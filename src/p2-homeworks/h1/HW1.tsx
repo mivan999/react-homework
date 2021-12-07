b3006296d54e8b58c8b7135579f77d3f31560d1f
Ivan Mulyavin <mivan999@mail.ru>
2021-12-07 17:06:22 +0700

    first homework

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import s from './Message.module.css'
 
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
     message: 'some text',
     time: '22:00',
+}
+
+function Message(props: MessageType) {
+    return (
+        <div className={s.message}  >
+            <img src={props.avatar} alt=""/>
+            <div className={s.receive}>
+                <div className={s.name}>{props.name} </div>
+                <div>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
+        </div>
+    )
 }
 
 function HW1() {
@@ -15,12 +35,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
